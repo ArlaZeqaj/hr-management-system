e2049e092a6e8cea07bebd4810214a6af04e3c3d
orkidagjeli <orkidagjeli15@gmail.com>
2025-04-03 00:26:32 +0200

    Employee-back

--- src/main/java/com/example/hrsystem/controller/EmployeeController.java
+++ src/main/java/com/example/hrsystem/controller/EmployeeController.java
@@ -1,22 +1,34 @@
 package com.example.hrsystem.controller;
 
-import org.springframework.web.bind.annotation.*;
 import com.example.hrsystem.model.Employee;
 import com.example.hrsystem.service.EmployeeService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
 
 @RestController
 @RequestMapping("/api/employees")
+@CrossOrigin(origins = "http://localhost:3000")
+
+
 public class EmployeeController {
 
-    private final EmployeeService employeeService;
+    private final EmployeeService service;
 
     public EmployeeController(EmployeeService employeeService) {
-        this.employeeService = employeeService;
+        this.service = employeeService;
     }
 
-    @CrossOrigin(origins = "http://localhost:3000")
-    @GetMapping("/random")
-    public Employee getRandomEmployee() {
-        return employeeService.getRandomEmployee();
+    @GetMapping
+    public List<Employee> getAllEmployees() {
+        try {
+            return service.getAllEmployees();
+        } catch (ExecutionException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
